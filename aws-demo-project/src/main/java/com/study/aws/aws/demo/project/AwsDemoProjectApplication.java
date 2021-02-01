0c388a5ec936fe2fa55774c13a9b9511e3138ee0
jinnings <jithinjoji@gmail.com>
2021-02-01 16:59:46 +0530

    Project Setup

---
+++ aws-demo-project/src/main/java/com/study/aws/aws/demo/project/AwsDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.aws.aws.demo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDemoProjectApplication.class, args);
+	}
+
+}
---
+++ aws-demo-project/src/test/java/com/study/aws/aws/demo/project/AwsDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aws.aws.demo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
